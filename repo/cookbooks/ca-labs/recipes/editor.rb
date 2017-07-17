c770fd59062a8cb9d120d0d79fc8bdae1ea637bd
Logan Rakai <lmrakai@gmail.com>
2017-07-17 15:14:07 -0600

    Step 3: Make editor recipe idempotent

--- repo/cookbooks/ca-labs/recipes/editor.rb
+++ repo/cookbooks/ca-labs/recipes/editor.rb
@@ -1,3 +1,5 @@
 windows_package 'notepad++' do
     source 'https://notepad-plus-plus.org/repository/7.x/7.4.2/npp.7.4.2.Installer.x64.exe'
-end
+    guard_interpreter :powershell_script
+    not_if "Test-Path 'C:\\Program Files\\Notepad++\\notepad++.exe'"
+ end
